a5fe80b405b4d1f4c5d9f7dd7e6776f871c2cd13
CR2429 <WDgaster.gtp@gmail.com>
2022-05-12 09:20:35 -0400

    Rangement et ajout de main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+###########################################################################
+### 420-2G2-HU - Programmation Orientée Objet
+### Travail: Projet de fin de session
+### Nom: Guillaume Paoli
+### Numero Étudiant: 2142678
+### Description du fichier: Fichier Main
+###########################################################################
+
+###################
+### IMPORTATION ###
+###################
+import ConnectionPage 
+
+##########################################################
+### DECLARATION DE VALEUR, DE LISTE ET DE DICTIONNAIRE ###
+##########################################################
+
+###############################
+### DECLARATION DE FONCTION ###
+###############################
+
+#################
+### PROGRAMME ###
+#################
